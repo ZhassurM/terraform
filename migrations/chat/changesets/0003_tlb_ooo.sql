9e251bcfb28597c55a8b54e45cf3f3a663fdcf30
ZhassurM <zhassurbek.muzaffarov@indriver.com>
2025-05-30 21:30:10 +0000

    test2

---
+++ migrations/chat/changesets/0003_tlb_ooo.sql
@@ -0,0 +1,8 @@
+
+--liquibase formatted sql
+
+--changeset asarkisov:DBAT-3582-1
+--liquibasePercona:usePercona="false"
+DELETE INTO chat (id, crowdin_key, enable);
+kkkkkk
+INSERT TABLE tbl_chat_roles7ориоакриаокри
