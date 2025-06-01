cf01ac430aedabd24577dd8890e3857993fa128d
ZhassurM <zhassurbek.muzaffarov@indriver.com>
2025-06-01 08:59:35 +0000

    ederferfe

---
+++ migrations/chat/changesets/kml.sql
@@ -0,0 +1,5 @@
+--liquibasePercona:usePercona="false"
+DELETE INTO chat (id, crowdin_key, enable);
+kkkkkk
+INSERT TABLE tbl_chat_roles7ориоакриаокри
+ALTER TABLE products ADD COLUMN created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
